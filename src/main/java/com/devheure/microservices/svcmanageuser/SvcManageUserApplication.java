674b6cfe86a5ee0052aeb7eca5c56e40f349692b
Hervé Darritchon <herve.darritchon@gmail.com>
2015-06-28 17:32:22 +0200

    Initial: Code from IntelliJ IDEA plugin for Spring-Boot.

---
+++ src/main/java/com/devheure/microservices/svcmanageuser/SvcManageUserApplication.java
@@ -0,0 +1,12 @@
+package com.devheure.microservices.svcmanageuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvcManageUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SvcManageUserApplication.class, args);
+    }
+}
